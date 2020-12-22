27042ffd357c98a9469892171728f844d1d62377
ShootMe <birreller@gmail.com>
2020-12-21 19:54:27 -0600

    2019-19

---
+++ Y2019/Puzzle19.cs
@@ -0,0 +1,62 @@
+using AdventOfCode.Core;
+using System.ComponentModel;
+namespace AdventOfCode.Y2019 {
+    public class Puzzle19 : ASolver {
+        private IntCode program;
+        public Puzzle19(string input) : base(input) { Name = "Tractor Beam"; }
+
+        public override void Setup() {
+            string[] elements = Input.Split(',');
+            long[] code = new long[elements.Length];
+            for (int i = 0; i < elements.Length; i++) {
+                code[i] = Tools.ParseLong(elements[i]);
+            }
+            program = new IntCode(code);
+        }
+
+        [Description("How many points are affected by the tractor beam in the 50x50 area?")]
+        public override string SolvePart1() {
+            int count = 0;
+            for (int i = 0; i < 50; i++) {
+                for (int j = 0; j < 50; j++) {
+                    if (CheckLocation(j, i)) {
+                        count++;
+                    }
+                }
+            }
+            return $"{count}";
+        }
+
+        [Description("Find a 100x100 sqaure, what value do you get if you do X * 10000 + Y?")]
+        public override string SolvePart2() {
+            program.Reset();
+
+            int max = 25 * 65;
+            for (int i = 17 * 65; i < max; i++) {
+                int j = i * 3 / 5;
+                bool isInBeam = false;
+
+                while (j < max && program.Output == 0) {
+                    isInBeam = CheckLocation(j++, i);
+                }
+
+                j--;
+                while (isInBeam) {
+                    bool rightCorner = CheckLocation(j + 99, i);
+                    if (rightCorner && CheckLocation(j, i + 99) && CheckLocation(j + 99, i + 99)) {
+                        return $"{j * 10000 + i}";
+                    }
+                    isInBeam = rightCorner && CheckLocation(++j, i);
+                }
+            }
+            return string.Empty;
+        }
+
+        private bool CheckLocation(int x, int y) {
+            program.Reset();
+            program.Run(x);
+            program.Run(y);
+            return program.Output == 1;
+        }
+    }
+}
