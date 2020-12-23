11434266dad76bc5916f8d9247ed077a3a118b7e
ShootMe <birreller@gmail.com>
2020-12-22 22:37:45 -0600

    2015-21 2015-22

--- Common/ArrayComparer.cs
+++ Common/ArrayComparer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 namespace AdventOfCode.Common {
-    public class ArrayComparer<T> : IEqualityComparer<T[]> where T : IEquatable<T> {
+    public class ArrayComparer<T> : IEqualityComparer<T[]>, IEqualityComparer<List<T>> where T : IEquatable<T> {
         public static ArrayComparer<T> Comparer { get; } = new ArrayComparer<T>();
         public bool Equals(T[] x, T[] y) {
             ReadOnlySpan<T> left = new ReadOnlySpan<T>(x);
@@ -23,6 +23,31 @@ namespace AdventOfCode.Common {
                 sb.Append(obj[i]).Append(", ");
             }
             if (obj.Length > 0) {
+                sb.Length -= 2;
+            }
+            return sb.ToString();
+        }
+
+        public bool Equals(List<T> x, List<T> y) {
+            if (x.Count != y.Count) { return false; }
+            for (int i = x.Count - 1; i >= 0; i--) {
+                if (!x[i].Equals(y[i])) { return false; }
+            }
+            return true;
+        }
+        public int GetHashCode(List<T> obj) {
+            int hash = 17;
+            for (int i = obj.Count - 1; i >= 0; i--) {
+                hash = (hash * 31) + obj[i].GetHashCode();
+            }
+            return hash;
+        }
+        public string ToString(List<T> obj) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < obj.Count; i++) {
+                sb.Append(obj[i]).Append(", ");
+            }
+            if (obj.Count > 0) {
                 sb.Length -= 2;
             }
             return sb.ToString();
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Solutions to the Advent of Code programming puzzles in C# .NET5
 https://adventofcode.com/
 
 ### Puzzles Completed
-  - 2015: 1-20
+  - 2015: 1-22
   - 2016: 1-19
   - 2017: 1-19
   - 2018: 1-14 & 16-19
