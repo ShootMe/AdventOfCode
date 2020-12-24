f93622d40ace96a22ed849faeb7612f4ef44cb5c
ShootMe <birreller@gmail.com>
2020-12-24 15:08:53 -0600

    2016-23 2016-24

--- Y2016/Puzzle12.cs
+++ Y2016/Puzzle12.cs
@@ -24,119 +24,140 @@ namespace AdventOfCode.Y2016 {
             program.Run();
             return $"{program.A.Value}";
         }
+    }
+    public class VM {
+        public List<Instruction> Instructions;
+        public Register A, B, C, D;
+        public int Pointer;
 
-        private class VM {
-            public List<Instruction> Instructions;
-            public Register A, B, C, D;
-            public int Pointer;
+        public VM(string program) {
+            List<string> lines = Tools.GetLines(program);
+            Instructions = new List<Instruction>(lines.Count);
+            A = new Register() { Name = "a", Value = 0 };
+            B = new Register() { Name = "b", Value = 0 };
+            C = new Register() { Name = "c", Value = 0 };
+            D = new Register() { Name = "d", Value = 0 };
 
-            public VM(string program) {
-                List<string> lines = Tools.GetLines(program);
-                Instructions = new List<Instruction>(lines.Count);
-                A = new Register() { Name = "a", Value = 0 };
-                B = new Register() { Name = "b", Value = 0 };
-                C = new Register() { Name = "c", Value = 0 };
-                D = new Register() { Name = "d", Value = 0 };
+            Dictionary<string, Register> registers = new Dictionary<string, Register>();
+            registers.Add("a", A);
+            registers.Add("b", B);
+            registers.Add("c", C);
+            registers.Add("d", D);
 
-                Dictionary<string, Register> registers = new Dictionary<string, Register>();
-                registers.Add("a", A);
-                registers.Add("b", B);
-                registers.Add("c", C);
-                registers.Add("d", D);
+            for (int i = 0; i < lines.Count; i++) {
+                string item = lines[i];
 
-                for (int i = 0; i < lines.Count; i++) {
-                    string item = lines[i];
+                int index1 = item.IndexOf(' ');
 
-                    int index1 = item.IndexOf(' ');
+                string type = item.Substring(0, index1);
 
-                    string type = item.Substring(0, index1);
+                int index2 = item.IndexOf(' ', index1 + 1);
+                if (index2 < 0) { index2 = item.Length; }
 
-                    int index2 = item.IndexOf(' ', index1 + 1);
-                    if (index2 < 0) { index2 = item.Length; }
-
-                    string value1 = item.Substring(index1 + 1, index2 - index1 - 1);
-                    string value2 = string.Empty;
-                    if (index2 < item.Length) {
-                        value2 = item.Substring(index2 + 1);
-                    }
-                    int val1;
-                    bool val1IsNumeric = int.TryParse(value1, out val1);
-
-                    switch (type) {
-                        case "cpy":
-                            Instructions.Add(new Instruction() {
-                                Code = OpCode.cpy,
-                                One = val1IsNumeric ? new Operand() { Value = val1 } : registers[value1],
-                                Two = registers[value2]
-                            });
-                            break;
-                        case "inc":
-                            Instructions.Add(new Instruction() {
-                                Code = OpCode.inc,
-                                One = registers[value1]
-                            });
-                            break;
-                        case "dec":
-                            Instructions.Add(new Instruction() {
-                                Code = OpCode.dec,
-                                One = registers[value1]
-                            });
-                            break;
-                        case "jnz":
-                            Instructions.Add(new Instruction() {
-                                Code = OpCode.jnz,
-                                One = val1IsNumeric ? new Operand() { Value = val1 } : registers[value1],
-                                Two = new Operand() { Value = int.Parse(value2) }
-                            });
-                            break;
-                    }
+                string value1 = item.Substring(index1 + 1, index2 - index1 - 1);
+                string value2 = string.Empty;
+                if (index2 < item.Length) {
+                    value2 = item.Substring(index2 + 1);
                 }
-            }
-            public void ClearRegisters() {
-                A.Value = 0;
-                B.Value = 0;
-                C.Value = 0;
-                D.Value = 0;
-            }
-            public void Run() {
-                Pointer = 0;
-                while (Pointer >= 0 && Pointer < Instructions.Count) {
-                    Instruction instruction = Instructions[Pointer];
-                    Pointer += instruction.Execute();
+                int val1;
+                bool val1IsNumeric = int.TryParse(value1, out val1);
+                int val2;
+                bool val2IsNumeric = int.TryParse(value2, out val2);
+
+                switch (type) {
+                    case "cpy":
+                        Instructions.Add(new Instruction() {
+                            Code = OpCode.cpy,
+                            One = val1IsNumeric ? new Operand() { Value = val1 } : registers[value1],
+                            Two = registers[value2]
+                        });
+                        break;
+                    case "inc":
+                        Instructions.Add(new Instruction() {
+                            Code = OpCode.inc,
+                            One = registers[value1]
+                        });
+                        break;
+                    case "dec":
+                        Instructions.Add(new Instruction() {
+                            Code = OpCode.dec,
+                            One = registers[value1]
+                        });
+                        break;
+                    case "jnz":
+                        Instructions.Add(new Instruction() {
+                            Code = OpCode.jnz,
+                            One = val1IsNumeric ? new Operand() { Value = val1 } : registers[value1],
+                            Two = val2IsNumeric ? new Operand() { Value = val2 } : registers[value2],
+                        });
+                        break;
+                    case "tgl":
+                        Instructions.Add(new Instruction() {
+                            Code = OpCode.tgl,
+                            One = val1IsNumeric ? new Operand() { Value = val1 } : registers[value1]
+                        });
+                        break;
                 }
             }
         }
-        private enum OpCode {
-            cpy,
-            inc,
-            dec,
-            jnz
+        public void ClearRegisters() {
+            A.Value = 0;
+            B.Value = 0;
+            C.Value = 0;
+            D.Value = 0;
         }
-        private class Instruction {
-            public OpCode Code;
-            public Operand One, Two;
-            public int Execute() {
-                switch (Code) {
-                    case OpCode.cpy: Two.Value = One.Value; break;
-                    case OpCode.inc: One.Value++; break;
-                    case OpCode.dec: One.Value--; break;
-                    case OpCode.jnz: if (One.Value != 0) { return Two.Value; } break;
+        public void Run() {
+            Pointer = 0;
+            while (Pointer >= 0 && Pointer < Instructions.Count) {
+                Instruction instruction = Instructions[Pointer];
+                Pointer += instruction.Execute(this);
+            }
+        }
+        public void ToggleInstruction(int relative) {
+            if (Pointer + relative >= 0 && Pointer + relative < Instructions.Count) {
+                Instruction instruction = Instructions[Pointer + relative];
+                switch (instruction.Code) {
+                    case OpCode.inc: instruction.Code = OpCode.dec; break;
+                    case OpCode.dec:
+                    case OpCode.tgl: instruction.Code = OpCode.inc; break;
+                    case OpCode.jnz: instruction.Code = OpCode.cpy; break;
+                    case OpCode.cpy: instruction.Code = OpCode.jnz; break;
                 }
-                return 1;
-            }
-            public override string ToString() {
-                return $"{Code} {One} {Two}";
             }
         }
-        private class Register : Operand {
-            public string Name;
-            public override string ToString() {
-                return $"{Name} {Value}";
+    }
+    public enum OpCode {
+        cpy,
+        inc,
+        dec,
+        jnz,
+        tgl
+    }
+    public class Instruction {
+        public OpCode Code;
+        public Operand One, Two;
+        public int Execute(VM program) {
+            switch (Code) {
+                case OpCode.cpy: if (Two is Register) { Two.Value = One.Value; } break;
+                case OpCode.inc: One.Value++; break;
+                case OpCode.dec: One.Value--; break;
+                case OpCode.jnz: if (One.Value != 0) { return Two.Value; } break;
+                case OpCode.tgl: program.ToggleInstruction(One.Value); break;
             }
+            return 1;
         }
-        private class Operand {
-            public int Value;
-            public virtual int Fetch() { return Value; }
+        public override string ToString() {
+            return $"{Code} {One} {Two}";
         }
+    }
+    public class Register : Operand {
+        public string Name;
+        public override string ToString() {
+            return $"{Name} {Value}";
+        }
+    }
+    public class Operand {
+        public int Value;
+        public override string ToString() { return $"{Value}"; }
     }
 }
