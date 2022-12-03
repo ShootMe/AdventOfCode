bd25de6a57691dbc4c4c11417c141a047ceb6e23
ShootMe <ShootMe>
2022-12-03 11:04:13 -0600

    Improvements

--- Y2022/Puzzle03.cs
+++ Y2022/Puzzle03.cs
@@ -1,5 +1,5 @@
 using AdventOfCode.Core;
-using System.Collections.Generic;
+using System;
 using System.ComponentModel;
 namespace AdventOfCode.Y2022 {
     [Description("Rucksack Reorganization")]
@@ -7,18 +7,21 @@ namespace AdventOfCode.Y2022 {
         [Description("What is the sum of the priorities of those item types?")]
         public override string SolvePart1() {
             int total = 0;
+            byte[] counts = new byte[53];
             foreach (string line in Input.Split('\n')) {
                 int halfway = line.Length / 2;
-                HashSet<char> items = new();
                 for (int i = 0; i < halfway; i++) {
-                    items.Add(line[i]);
+                    counts[ItemValue(line[i])] = 1;
                 }
+
                 for (int i = halfway; i < line.Length; i++) {
-                    if (items.Contains(line[i])) {
-                        total += line[i] - (line[i] >= 'a' ? 'a' - 1 : 'A' - 27);
+                    int value = ItemValue(line[i]);
+                    if (counts[value] != 0) {
+                        total += value;
                         break;
                     }
                 }
+                Array.Clear(counts);
             }
             return $"{total}";
         }
@@ -26,18 +29,34 @@ namespace AdventOfCode.Y2022 {
         [Description("What is the sum of the priorities of those item types?")]
         public override string SolvePart2() {
             int total = 0;
+            byte[] counts = new byte[53];
             string[] lines = Input.Split('\n');
             for (int i = 0; i < lines.Length; i += 3) {
-                HashSet<char> items = new(lines[i]);
-                HashSet<char> other = new(lines[i + 1]);
-                items.IntersectWith(other);
-                other = new(lines[i + 2]);
-                items.IntersectWith(other);
-                foreach (char c in items) {
-                    total += c - (c >= 'a' ? 'a' - 1 : 'A' - 27);
+                string line = lines[i];
+                for (int j = 0; j < line.Length; j++) {
+                    counts[ItemValue(line[j])] = 1;
                 }
+
+                line = lines[i + 1];
+                for (int j = 0; j < line.Length; j++) {
+                    counts[ItemValue(line[j])] |= 2;
+                }
+
+                line = lines[i + 2];
+                for (int j = 0; j < line.Length; j++) {
+                    int value = ItemValue(line[j]);
+                    if (counts[value] == 3) {
+                        total += value;
+                        break;
+                    }
+                }
+                Array.Clear(counts);
             }
             return $"{total}";
+        }
+
+        private int ItemValue(char c) {
+            return (c & 0x1f) + (c >= 'a' ? 0 : 26);
         }
     }
 }
