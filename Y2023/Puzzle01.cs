85c5e8e2924054b1f8ffad464ee3c829e263485a
ShootMe <ShootMe>
2023-12-01 10:59:26 -0600

    2023-01 Improvements

--- Y2023/Puzzle01.cs
+++ Y2023/Puzzle01.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 namespace AdventOfCode.Y2023 {
     [Description("Trebuchet?!")]
     public class Puzzle01 : ASolver {
-        private string[] digits = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         private List<string> calibrations = new();
 
         public override void Setup() {
@@ -15,46 +14,78 @@ namespace AdventOfCode.Y2023 {
 
         [Description("What is the sum of all of the calibration values?")]
         public override string SolvePart1() {
-            int total = 0;
-            for (int i = 0; i < calibrations.Count; i++) {
-                string calibration = calibrations[i];
-                total += GetFirstCalibrationDigit(calibration, 10) * 10 + GetSecondCalibrationDigit(calibration, 10);
-            }
-            return $"{total}";
+            return $"{FindTotalDigitSum(false)}";
         }
 
         [Description("What is the sum of all of the calibration values?")]
         public override string SolvePart2() {
+            return $"{FindTotalDigitSum(true)}";
+        }
+        private int FindTotalDigitSum(bool includeLetters) {
             int total = 0;
             for (int i = 0; i < calibrations.Count; i++) {
                 string calibration = calibrations[i];
-                total += GetFirstCalibrationDigit(calibration, 20) * 10 + GetSecondCalibrationDigit(calibration, 20);
-            }
-            return $"{total}";
-        }
-        private int GetFirstCalibrationDigit(string calibration, int maxStrings) {
-            int minimum = calibration.Length;
-            int firstDigit = 0;
-            for (int i = 0; i < maxStrings; i++) {
-                int index = calibration.IndexOf(digits[i]);
-                if (index >= 0 && index < minimum) {
-                    minimum = index;
-                    firstDigit = i;
+                DigitWindow window = new DigitWindow();
+                for (int j = 0; j < calibration.Length; j++) {
+                    char c = calibration[j];
+                    int value = window.FindStart(c, includeLetters);
+                    if (value >= 0) { total += value * 10; break; }
+                }
+                window = new DigitWindow();
+                for (int j = calibration.Length - 1; j >= 0; j--) {
+                    char c = calibration[j];
+                    int value = window.FindEnd(c, includeLetters);
+                    if (value >= 0) { total += value; break; }
                 }
             }
-            return firstDigit % 10;
+            return total;
         }
-        private int GetSecondCalibrationDigit(string calibration, int maxStrings) {
-            int maximum = -1;
-            int secondDigit = 0;
-            for (int i = 0; i < maxStrings; i++) {
-                int index = calibration.LastIndexOf(digits[i]);
-                if (index >= 0 && index > maximum) {
-                    maximum = index;
-                    secondDigit = i;
-                }
+        private struct DigitWindow {
+            public char D1, D2, D3, D4, D5;
+            public int FindStart(char current, bool includeLetters = false) {
+                D5 = D4;
+                D4 = D3;
+                D3 = D2;
+                D2 = D1;
+                D1 = current;
+                return (D1, D2, D3, D4, D5, includeLetters) switch {
+                    ( >= '0' and <= '9', _, _, _, _, _) => D1 & 15,
+                    (_, _, _, _, _, false) => -1,
+                    ('o', 'r', 'e', 'z', _, _) => 0,
+                    ('e', 'n', 'o', _, _, _) => 1,
+                    ('o', 'w', 't', _, _, _) => 2,
+                    ('e', 'e', 'r', 'h', 't', _) => 3,
+                    ('r', 'u', 'o', 'f', _, _) => 4,
+                    ('e', 'v', 'i', 'f', _, _) => 5,
+                    ('x', 'i', 's', _, _, _) => 6,
+                    ('n', 'e', 'v', 'e', 's', _) => 7,
+                    ('t', 'h', 'g', 'i', 'e', _) => 8,
+                    ('e', 'n', 'i', 'n', _, _) => 9,
+                    _ => -1
+                };
             }
-            return secondDigit % 10;
+            public int FindEnd(char current, bool includeLetters = false) {
+                D5 = D4;
+                D4 = D3;
+                D3 = D2;
+                D2 = D1;
+                D1 = current;
+                return (D1, D2, D3, D4, D5, includeLetters) switch {
+                    ( >= '0' and <= '9', _, _, _, _, _) => D1 & 15,
+                    (_, _, _, _, _, false) => -1,
+                    ('z', 'e', 'r', 'o', _, _) => 0,
+                    ('o', 'n', 'e', _, _, _) => 1,
+                    ('t', 'w', 'o', _, _, _) => 2,
+                    ('t', 'h', 'r', 'e', 'e', _) => 3,
+                    ('f', 'o', 'u', 'r', _, _) => 4,
+                    ('f', 'i', 'v', 'e', _, _) => 5,
+                    ('s', 'i', 'x', _, _, _) => 6,
+                    ('s', 'e', 'v', 'e', 'n', _) => 7,
+                    ('e', 'i', 'g', 'h', 't', _) => 8,
+                    ('n', 'i', 'n', 'e', _, _) => 9,
+                    _ => -1
+                };
+            }
         }
     }
 }
