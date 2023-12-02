16a53433af6f30537b0c4dbb969ce73847511897
ShootMe <ShootMe>
2023-12-01 22:08:12 -0600

    2023-01 cleanup

--- Y2023/Puzzle01.cs
+++ Y2023/Puzzle01.cs
@@ -29,13 +29,13 @@ namespace AdventOfCode.Y2023 {
                 for (int j = 0; j < calibration.Length; j++) {
                     char c = calibration[j];
                     int value = window.FindStart(c, includeLetters);
-                    if (value >= 0) { total += value * 10; break; }
+                    if (value > 0) { total += value * 10; break; }
                 }
                 window = new DigitWindow();
                 for (int j = calibration.Length - 1; j >= 0; j--) {
                     char c = calibration[j];
                     int value = window.FindEnd(c, includeLetters);
-                    if (value >= 0) { total += value; break; }
+                    if (value > 0) { total += value; break; }
                 }
             }
             return total;
@@ -48,20 +48,20 @@ namespace AdventOfCode.Y2023 {
                 D3 = D2;
                 D2 = D1;
                 D1 = current;
-                return (D1, D2, D3, D4, D5, includeLetters) switch {
-                    ( >= '0' and <= '9', _, _, _, _, _) => D1 & 15,
-                    (_, _, _, _, _, false) => -1,
-                    ('o', 'r', 'e', 'z', _, _) => 0,
-                    ('e', 'n', 'o', _, _, _) => 1,
-                    ('o', 'w', 't', _, _, _) => 2,
-                    ('e', 'e', 'r', 'h', 't', _) => 3,
-                    ('r', 'u', 'o', 'f', _, _) => 4,
-                    ('e', 'v', 'i', 'f', _, _) => 5,
-                    ('x', 'i', 's', _, _, _) => 6,
-                    ('n', 'e', 'v', 'e', 's', _) => 7,
-                    ('t', 'h', 'g', 'i', 'e', _) => 8,
-                    ('e', 'n', 'i', 'n', _, _) => 9,
-                    _ => -1
+                if (char.IsDigit(D1)) { return D1 & 15; }
+                if (!includeLetters) { return 0; }
+
+                return (D1, D2, D3, D4, D5) switch {
+                    ('e', 'n', 'o', _, _) => 1,
+                    ('o', 'w', 't', _, _) => 2,
+                    ('e', 'e', 'r', 'h', 't') => 3,
+                    ('r', 'u', 'o', 'f', _) => 4,
+                    ('e', 'v', 'i', 'f', _) => 5,
+                    ('x', 'i', 's', _, _) => 6,
+                    ('n', 'e', 'v', 'e', 's') => 7,
+                    ('t', 'h', 'g', 'i', 'e') => 8,
+                    ('e', 'n', 'i', 'n', _) => 9,
+                    _ => 0
                 };
             }
             public int FindEnd(char current, bool includeLetters = false) {
@@ -70,20 +70,20 @@ namespace AdventOfCode.Y2023 {
                 D3 = D2;
                 D2 = D1;
                 D1 = current;
-                return (D1, D2, D3, D4, D5, includeLetters) switch {
-                    ( >= '0' and <= '9', _, _, _, _, _) => D1 & 15,
-                    (_, _, _, _, _, false) => -1,
-                    ('z', 'e', 'r', 'o', _, _) => 0,
-                    ('o', 'n', 'e', _, _, _) => 1,
-                    ('t', 'w', 'o', _, _, _) => 2,
-                    ('t', 'h', 'r', 'e', 'e', _) => 3,
-                    ('f', 'o', 'u', 'r', _, _) => 4,
-                    ('f', 'i', 'v', 'e', _, _) => 5,
-                    ('s', 'i', 'x', _, _, _) => 6,
-                    ('s', 'e', 'v', 'e', 'n', _) => 7,
-                    ('e', 'i', 'g', 'h', 't', _) => 8,
-                    ('n', 'i', 'n', 'e', _, _) => 9,
-                    _ => -1
+                if (char.IsDigit(D1)) { return D1 & 15; }
+                if (!includeLetters) { return 0; }
+
+                return (D1, D2, D3, D4, D5) switch {
+                    ('o', 'n', 'e', _, _) => 1,
+                    ('t', 'w', 'o', _, _) => 2,
+                    ('t', 'h', 'r', 'e', 'e') => 3,
+                    ('f', 'o', 'u', 'r', _) => 4,
+                    ('f', 'i', 'v', 'e', _) => 5,
+                    ('s', 'i', 'x', _, _) => 6,
+                    ('s', 'e', 'v', 'e', 'n') => 7,
+                    ('e', 'i', 'g', 'h', 't') => 8,
+                    ('n', 'i', 'n', 'e', _) => 9,
+                    _ => 0
                 };
             }
         }
