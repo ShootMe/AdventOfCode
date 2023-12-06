fbba6548065ee0336eaa7ab83db54fc81c71be35
ShootMe <ShootMe>
2023-12-06 09:57:09 -0600

    2023-06 cleanup

--- Y2023/Puzzle06.cs
+++ Y2023/Puzzle06.cs
@@ -16,56 +16,33 @@ namespace AdventOfCode.Y2023 {
             string[] boatTimes = lines[0].Substring(5).Split(' ', StringSplitOptions.RemoveEmptyEntries);
             string[] boatDistances = lines[1].Substring(9).Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            string timeStr = string.Empty;
-            string distanceStr = string.Empty;
             for (int i = 0; i < boatTimes.Length; i++) {
                 times.Add(boatTimes[i].ToInt());
                 distances.Add(boatDistances[i].ToInt());
-                timeStr += boatTimes[i];
-                distanceStr += boatDistances[i];
             }
 
-            timeAll = timeStr.ToLong();
-            distanceAll = distanceStr.ToLong();
+            timeAll = lines[0].ToLong();
+            distanceAll = lines[1].ToLong();
         }
 
         [Description("Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?")]
         public override string SolvePart1() {
             int total = 1;
             for (int i = 0; i < times.Count; i++) {
-                int time = times[i];
-                int distance = distances[i];
-                int timesBeat = 0;
-                for (int j = 1; j < time; j++) {
-                    if ((time - j) * j > distance) {
-                        timesBeat++;
-                    }
-                }
-                total *= timesBeat;
+                total *= CalculateWins(times[i], distances[i]);
             }
             return $"{total}";
         }
 
         [Description("How many ways can you beat the record in this one much longer race?")]
         public override string SolvePart2() {
-            int div = (int)(distanceAll / timeAll);
-            int startTime = 0;
-            for (int i = div; i < timeAll; i++) {
-                if ((timeAll - i) * i > distanceAll) {
-                    startTime = i;
-                    break;
-                }
-            }
+            return $"{CalculateWins(timeAll, distanceAll)}";
+        }
 
-            int endTime = 0;
-            for (int i = (int)timeAll - div; i > startTime; i--) {
-                if ((timeAll - i) * i > distanceAll) {
-                    endTime = i;
-                    break;
-                }
-            }
-
-            return $"{endTime - startTime + 1}";
+        public int CalculateWins(long time, long distance) {
+            int maxVal = (int)Math.Ceiling((time + Math.Sqrt(time * time - 4 * distance)) / 2d);
+            int minVal = (int)((time - Math.Sqrt(time * time - 4 * distance)) / 2d);
+            return maxVal - minVal - 1;
         }
     }
 }
