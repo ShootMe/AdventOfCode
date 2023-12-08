d2119eda88624b2140680fdd4ae21b7cdb90abfa
ShootMe <ShootMe>
2023-12-08 15:09:14 -0600

    2023-08 cleanup

--- Y2023/Puzzle08.cs
+++ Y2023/Puzzle08.cs
@@ -1,37 +1,52 @@
 using AdventOfCode.Common;
 using AdventOfCode.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 namespace AdventOfCode.Y2023 {
     [Description("Haunted Wasteland")]
     public class Puzzle08 : ASolver {
-        private List<Node> nodes = new();
+        private List<Node> startNodes = new();
         private string directions;
         public override void Setup() {
-            Node.ALL.Clear();
             string[] lines = Input.Split('\n');
             directions = lines[0];
+            Dictionary<string, Node> allNodes = new(StringComparer.OrdinalIgnoreCase);
+            for (int i = 2; i < lines.Length; i++) {
+                string line = lines[i];
+                int index = line.IndexOf(' ');
+                string name = line.Substring(0, index);
+                Node node = new Node(name);
+                if (node.Type == NodeType.Start) {
+                    if (node.Name == "AAA") {
+                        startNodes.Insert(0, node);
+                    } else {
+                        startNodes.Add(node);
+                    }
+                }
+                allNodes.Add(name, node);
+            }
             for (int i = 2; i < lines.Length; i++) {
                 string line = lines[i];
                 string[] splits = line.SplitOn(" = (", ", ", ")");
-                nodes.Add(new Node(splits[0], splits[1], splits[2]));
+                Node node = allNodes[splits[0]];
+                node.Left = allNodes[splits[1]];
+                node.Right = allNodes[splits[2]];
             }
         }
 
         [Description("How many steps are required to reach ZZZ?")]
         public override string SolvePart1() {
-            return $"{GetStepCount(Node.ALL["AAA"])}";
+            return $"{GetStepCount(startNodes[0])}";
         }
 
         [Description("How many steps does it take before you're only on nodes that end with Z?")]
         public override string SolvePart2() {
             long total = 1;
-            for (int i = 0; i < nodes.Count; i++) {
-                Node node = nodes[i];
-                if (node.Type == NodeType.Start) {
-                    long steps = GetStepCount(node, true);
-                    total *= steps / Extensions.GCD(total, steps);
-                }
+            for (int i = 0; i < startNodes.Count; i++) {
+                Node node = startNodes[i];
+                long steps = GetStepCount(node, true);
+                total *= steps / Extensions.GCD(total, steps);
             }
             return $"{total}";
         }
@@ -42,8 +57,8 @@ namespace AdventOfCode.Y2023 {
                 char c = directions[index++];
                 total++;
                 switch (c) {
-                    case 'R': node = node.RightNode; break;
-                    case 'L': node = node.LeftNode; break;
+                    case 'R': node = node.Right; break;
+                    case 'L': node = node.Left; break;
                 }
                 if ((!endsInZ && node.Name == "ZZZ") || (endsInZ && node.Type == NodeType.End)) { break; }
                 if (index >= directions.Length) { index = 0; }
@@ -52,18 +67,13 @@ namespace AdventOfCode.Y2023 {
         }
 
         private class Node {
-            public static Dictionary<string, Node> ALL = new Dictionary<string, Node>();
-            public string Name, Left, Right;
+            public string Name;
+            public Node Left, Right;
             public NodeType Type;
-            public Node(string name, string left, string right) {
+            public Node(string name) {
                 Name = name;
                 Type = name.EndsWith('A') ? NodeType.Start : name.EndsWith('Z') ? NodeType.End : NodeType.Normal;
-                Left = left;
-                Right = right;
-                ALL.Add(name, this);
             }
-            public Node RightNode { get { return ALL[Right]; } }
-            public Node LeftNode { get { return ALL[Left]; } }
         }
         private enum NodeType {
             Start,
