7933a29d279bb35a30fc3d99c6a8d5d9bef0999b
ShootMe <birreller@gmail.com>
2024-12-07 10:52:02 -0600

    2024 Day 7

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ https://adventofcode.com/
  4 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: |
  5 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: |
  6 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: |
- 7 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | |
+ 7 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: |
  8 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | |
  9 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | |
  10 | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | :star: :star: | |
---
+++ Y2024/Puzzle07.cs
@@ -0,0 +1,71 @@
+using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace AdventOfCode.Y2024 {
+    [Description("Bridge Repair")]
+    public class Puzzle07 : ASolver {
+        private List<Formula> formulas = new();
+
+        public override void Setup() {
+            string[] lines = Input.Split('\n');
+            foreach (string line in lines) {
+                formulas.Add(new Formula(line));
+            }
+        }
+
+        [Description("What is their total calibration result?")]
+        public override string SolvePart1() {
+            long total = 0;
+            for (int i = 0; i < formulas.Count; i++) {
+                Formula formula = formulas[i];
+                total += formula.HasValidOperators() ? formula.Value : 0L;
+            }
+            return $"{total}";
+        }
+
+        [Description("What is their total calibration result?")]
+        public override string SolvePart2() {
+            long total = 0;
+            for (int i = 0; i < formulas.Count; i++) {
+                Formula formula = formulas[i];
+                total += formula.HasValidOperators(true) ? formula.Value : 0L;
+            }
+            return $"{total}";
+        }
+
+        private class Formula {
+            public long Value;
+            public List<int> Operands = new();
+            public Formula(string formula) {
+                int index = formula.IndexOf(':');
+                Value = formula[0..index].ToLong();
+                string[] operands = formula[(index + 1)..].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < operands.Length; i++) {
+                    Operands.Add(operands[i].ToInt());
+                }
+            }
+            public bool HasValidOperators(bool use3 = false) {
+                return TryNext(1, Operands[0], use3) == Value;
+            }
+            private long TryNext(int index, long value, bool use3) {
+                if (index < Operands.Count) {
+                    long operand = Operands[index];
+                    if (TryNext(index + 1, value + operand, use3) == Value || TryNext(index + 1, value * operand, use3) == Value) {
+                        return Value;
+                    }
+                    if (use3) {
+                        long mult = (long)Math.Pow(10, (long)Math.Log10(operand) + 1);
+                        if (TryNext(index + 1, value * mult + operand, use3) == Value) {
+                            return Value;
+                        }
+                    }
+                } else {
+                    return value;
+                }
+                return -value;
+            }
+        }
+    }
+}
