71ac4a88f0967d533fb124ad298d2b3ef3d17d58
ShootMe <birreller@gmail.com>
2024-12-11 09:54:08 -0600

    2024 Day 11 Improvements

--- Y2024/Puzzle11.cs
+++ Y2024/Puzzle11.cs
@@ -6,7 +6,6 @@ namespace AdventOfCode.Y2024 {
     [Description("Plutonian Pebbles")]
     public class Puzzle11 : ASolver {
         private List<long> stones = new();
-        private Dictionary<(long, int), long> lookup = new();
         public override void Setup() {
             string[] values = Input.Split(' ');
             foreach (string value in values) {
@@ -16,48 +15,59 @@ namespace AdventOfCode.Y2024 {
 
         [Description("How many stones will you have after blinking 25 times?")]
         public override string SolvePart1() {
-            long total = 0;
-            for (int j = 0; j < stones.Count; j++) {
-                long stone = stones[j];
-                total += NextStone(stone, 25);
-            }
-            return $"{total}";
+            return $"{FindTotalStones(25)}";
         }
-        
+
         [Description("How many stones would you have after blinking a total of 75 times?")]
         public override string SolvePart2() {
-            long total = 0;
-            for (int j = 0; j < stones.Count; j++) {
-                long stone = stones[j];
-                total += NextStone(stone, 75);
-            }
-            return $"{total}";
+            return $"{FindTotalStones(75)}";
         }
 
-        private long NextStone(long stone, int level) {
-            if (level == 0) { return 1; }
-
-            long value;
-            if (lookup.TryGetValue((stone, level), out value)) {
-                return value;
+        private long FindTotalStones(int blinks) {
+            Dictionary<long, long> counts = new();
+            for (int j = 0; j < stones.Count; j++) {
+                UpdateCounts(counts, stones[j], 1);
             }
 
-            if (stone == 0) {
-                value = NextStone(1, level - 1);
-            } else if (stone == 1) {
-                value = NextStone(2024, level - 1);
+            Dictionary<long, long> newCounts = new();
+            for (int i = 0; i < blinks; i++) {
+                newCounts.Clear();
+                foreach (var stone in counts) {
+                    UpdateStone(stone, newCounts);
+                }
+                Dictionary<long, long> temp = counts;
+                counts = newCounts;
+                newCounts = temp;
+            }
+
+            long total = 0;
+            foreach (long value in counts.Values) {
+                total += value;
+            }
+
+            return total;
+        }
+        private static void UpdateCounts(Dictionary<long, long> counts, long newStone, long amount) {
+            if (!counts.TryAdd(newStone, amount)) {
+                counts[newStone] += amount;
+            }
+        }
+        private void UpdateStone(KeyValuePair<long, long> stone, Dictionary<long, long> counts) {
+            if (stone.Key == 0) {
+                UpdateCounts(counts, 1, stone.Value);
+            } else if (stone.Key == 1) {
+                UpdateCounts(counts, 2024, stone.Value);
             } else {
-                string stoneString = stone.ToString();
+                string stoneString = stone.Key.ToString();
                 if ((stoneString.Length & 1) == 0) {
-                    value = NextStone(stoneString.Substring(0, stoneString.Length / 2).ToLong(), level - 1) +
-                       NextStone(stoneString.Substring(stoneString.Length / 2).ToLong(), level - 1);
+                    long next = stoneString.Substring(0, stoneString.Length / 2).ToLong();
+                    UpdateCounts(counts, next, stone.Value);
+                    next = stoneString.Substring(stoneString.Length / 2).ToLong();
+                    UpdateCounts(counts, next, stone.Value);
                 } else {
-                    value = NextStone(stone * 2024, level - 1);
+                    UpdateCounts(counts, stone.Key * 2024, stone.Value);
                 }
             }
-            lookup.Add((stone, level), value);
-
-            return value;
         }
     }
 }
