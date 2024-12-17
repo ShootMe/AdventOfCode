2a662c9b6068a3067645b0046303f680c2da553d
ShootMe <birreller@gmail.com>
2024-12-16 18:21:19 -0600

    2024 Day 16 Improvements

--- Y2024/Puzzle16.cs
+++ Y2024/Puzzle16.cs
@@ -1,15 +1,13 @@
-using AdventOfCode.Common;
 using AdventOfCode.Core;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 namespace AdventOfCode.Y2024 {
     [Description("Reindeer Maze")]
-    //[Run("Example")]
-    //[Run("Given")]
     public class Puzzle16 : ASolver {
         private char[,] map;
-        private int width, height, startX, startY, endX, endY;
+        private int width, height, endX, endY;
+        private Queue<(int, int, int, int)> open = new();
+        private Dictionary<(int, int), int> seen = new();
 
         public override void Setup() {
             string[] lines = Input.Split('\n');
@@ -17,6 +15,7 @@ namespace AdventOfCode.Y2024 {
             width = lines[0].Length;
             map = new char[height, width];
 
+            int startX = 0, startY = 0;
             for (int y = 0; y < height; y++) {
                 string line = lines[y];
                 for (int x = 0; x < width; x++) {
@@ -29,15 +28,13 @@ namespace AdventOfCode.Y2024 {
                     }
                 }
             }
+
+            open.Enqueue((startX, startY, 0, 0));
+            seen.Add((startX, startY), 0);
         }
 
         [Description("What is the lowest score a Reindeer could possibly get?")]
         public override string SolvePart1() {
-            Dictionary<(int, int), int> seen = new();
-            Queue<(int, int, int, int)> open = new();
-            open.Enqueue((startX, startY, 0, 0));
-            seen.Add((startX, startY), 0);
-
             void AddNext(int x, int y, int dir, int score) {
                 if (dir < 0) { dir += 4; } else if (dir > 3) { dir -= 4; }
                 switch (dir) {
@@ -57,11 +54,14 @@ namespace AdventOfCode.Y2024 {
             }
 
             int minScore = int.MaxValue;
+            int minDir = int.MaxValue;
             while (open.Count > 0) {
                 (int x, int y, int dir, int score) = open.Dequeue();
+
                 if (map[y, x] == 'E') {
                     if (score < minScore) {
                         minScore = score;
+                        minDir = dir;
                     }
                     continue;
                 }
@@ -70,60 +70,34 @@ namespace AdventOfCode.Y2024 {
                 AddNext(x, y, dir + 1, score + 1001);
                 AddNext(x, y, dir - 1, score + 1001);
             }
+
+            open.Enqueue((endX, endY, minDir, minScore));
             return $"{minScore}";
         }
 
         [Description("How many tiles are part of at least one of the best paths through the maze?")]
         public override string SolvePart2() {
-            Dictionary<(int, int, int), int> seen = new();
-            Queue<Path> open = new();
-            open.Enqueue(new Path(startX, startY, 0, 0, null));
-            seen.Add((startX, startY, 0), 0);
-
-            void AddNext(Path current, int dir, int score) {
-                int x = current.X, y = current.Y;
+            HashSet<(int, int)> bestPath = new();
+            void CheckPrevious(int x, int y, int dir, int score) {
                 if (dir < 0) { dir += 4; } else if (dir > 3) { dir -= 4; }
                 switch (dir) {
-                    case 0: x++; break;
-                    case 1: y++; break;
-                    case 2: x--; break;
-                    case 3: y--; break;
+                    case 0: x--; break;
+                    case 1: y--; break;
+                    case 2: x++; break;
+                    case 3: y++; break;
                 }
-                if (map[y, x] == '#') { return; }
 
-                score += current.Score;
-                int oldScore = 0;
-                bool exists = seen.TryGetValue((x, y, dir), out oldScore);
-                if (!exists || oldScore >= score) {
-                    seen[(x, y, dir)] = score;
-                    open.Enqueue(new Path(x, y, dir, score, current));
-                }
+                if (map[y, x] == '#' || !seen.TryGetValue((x, y), out int prevScore) || prevScore > score) { return; }
+                bestPath.Add((x, y));
+                open.Enqueue((x, y, dir, score));
             }
 
-            HashSet<(int, int)> bestPath = new();
-            int minScore = int.MaxValue;
             while (open.Count > 0) {
-                Path current = open.Dequeue();
-                if (map[current.Y, current.X] == 'E') {
-                    if (current.Score < minScore) {
-                        minScore = current.Score;
-                        bestPath.Clear();
-                    }
-                    if (current.Score == minScore) {
-                        Path temp = current;
-                        int count = 0;
-                        while (temp != null) {
-                            bestPath.Add((temp.X, temp.Y));
-                            temp = temp.Last;
-                            count++;
-                        }
-                    }
-                    continue;
-                }
-
-                AddNext(current, current.Dir, 1);
-                AddNext(current, current.Dir + 1, 1001);
-                AddNext(current, current.Dir - 1, 1001);
+                (int x, int y, int dir, int score) = open.Dequeue();
+                bestPath.Add((x, y));
+                CheckPrevious(x, y, dir, score - 1);
+                CheckPrevious(x, y, dir + 1, score - 1001);
+                CheckPrevious(x, y, dir - 1, score - 1001);
             }
             return $"{bestPath.Count}";
         }
