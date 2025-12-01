5dfbcfc7eb3561d72f627a3a812f650284df0d5a
ShootMe <birreller@gmail.com>
2025-12-01 11:22:02 -0600

    2025-1

--- Program.cs
+++ Program.cs
@@ -5,18 +5,12 @@ namespace AdventOfCode {
             //Synacor Challenge
             //Synacor.Emulator.RunChallenge();
 
-            //Run code for all years and days
-            //Runner.AdventYear();
-
-            //Run code for a specific year
-            //Runner.AdventYear(2023);
-
             //Run code for a specific day in the year
             //Class is auto generated if it doesn't exist and
             //will auto download description and input if current date > specified and they don't exist
             //or within 30 minutes of the problem start (and will wait till midnight EST)
             //Problem description will be brought up in the default browser if it was pulled
-            Runner.AdventYear(2024, 1);
+            Runner.AdventYear(2025, 1);
         }
     }
 }
---
+++ Y2025/Puzzle01.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using AdventOfCode.Common;
+using AdventOfCode.Core;
+namespace AdventOfCode.Y2025 {
+    [Description("Secret Entrance")]
+    public class Puzzle01 : ASolver {
+        private List<int> rotations = new();
+
+        public override void Setup() {
+            string[] lines = Input.Split('\n');
+            foreach (string line in lines) {
+                int num = line.ToInt();
+                if (line[0] == 'L') { num = -num; }
+                rotations.Add(num);
+            }
+        }
+
+        [Description("What's the actual password to open the door?")]
+        public override string SolvePart1() {
+            int position = 50;
+            int password = 0;
+            for (int i = 0; i < rotations.Count; i++) {
+                position += rotations[i] % 100;
+                if (position < 0) { position += 100; }
+                if (position >= 100) { position -= 100; }
+                if (position == 0) { password++; }
+            }
+            return $"{password}";
+        }
+
+        [Description("Using password method 0x434C49434B, what is the password to open the door?")]
+        public override string SolvePart2() {
+            int position = 50;
+            int password = 0;
+            for (int i = 0; i < rotations.Count; i++) {
+                int num = rotations[i];
+                int add = num < 0 ? -1 : 1;
+                while (num != 0) {
+                    position += add;
+                    if (position < 0) { position += 100; }
+                    if (position == 100) { position -= 100; }
+                    if (position == 0) { password++; }
+                    num -= add;
+                }
+            }
+            return $"{password}";
+        }
+    }
+}
