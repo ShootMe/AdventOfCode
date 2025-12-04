20099e729c1b66409b9a3c29ee1eedc9e1b449f5
ShootMe <birreller@gmail.com>
2025-12-04 10:53:13 -0600

    2025-4 improvements

--- Y2025/Puzzle04.cs
+++ Y2025/Puzzle04.cs
@@ -4,18 +4,18 @@ using AdventOfCode.Core;
 namespace AdventOfCode.Y2025 {
     [Description("Printing Department")]
     public class Puzzle04 : ASolver {
-        private int[,] floor;
+        private bool[,] floor;
         private int width, height;
 
         public override void Setup() {
             string[] lines = Input.Split('\n');
             width = lines[0].Length;
             height = lines.Length;
-            floor = new int[height + 2, width + 2];
+            floor = new bool[height + 2, width + 2];
             for (int y = 1; y <= height; y++) {
                 string line = lines[y - 1];
                 for (int x = 1; x <= width; x++) {
-                    floor[y, x] = line[x - 1] == '@' ? 1 : 0;
+                    floor[y, x] = line[x - 1] == '@';
                 }
             }
         }
@@ -24,7 +24,7 @@ namespace AdventOfCode.Y2025 {
         public override string SolvePart1() {
             return $"{RemoveRolls(false)}";
         }
-        
+
         [Description("How many rolls of paper in total can be removed by the Elves and their forklifts?")]
         public override string SolvePart2() {
             int total = 0;
@@ -41,29 +41,26 @@ namespace AdventOfCode.Y2025 {
             List<(int, int)> toRemove = new();
             for (int y = 1; y <= height; y++) {
                 for (int x = 1; x <= width; x++) {
-                    if (floor[y, x] != 1) { continue; }
+                    if (!floor[y, x]) { continue; }
 
                     int count = 0;
-                    count += floor[y - 1, x - 1] != 0 ? 1 : 0;
-                    count += floor[y - 1, x] != 0 ? 1 : 0;
-                    count += floor[y - 1, x + 1] != 0 ? 1 : 0;
-                    count += floor[y, x - 1] != 0 ? 1 : 0;
-                    count += floor[y, x + 1] != 0 ? 1 : 0;
-                    count += floor[y + 1, x - 1] != 0 ? 1 : 0;
-                    count += floor[y + 1, x] != 0 ? 1 : 0;
-                    count += floor[y + 1, x + 1] != 0 ? 1 : 0;
+                    count += floor[y - 1, x - 1] ? 1 : 0;
+                    count += floor[y - 1, x] ? 1 : 0;
+                    count += floor[y - 1, x + 1] ? 1 : 0;
+                    count += floor[y, x - 1] ? 1 : 0;
+                    count += floor[y, x + 1] ? 1 : 0;
+                    count += floor[y + 1, x - 1] ? 1 : 0;
+                    count += floor[y + 1, x] ? 1 : 0;
+                    count += floor[y + 1, x + 1] ? 1 : 0;
                     if (count < 4) {
                         rolls++;
-                        if (remove) {
-                            floor[y, x] = -1;
-                            toRemove.Add((y, x));
-                        }
+                        if (remove) { toRemove.Add((y, x)); }
                     }
                 }
             }
             for (int i = 0; i < toRemove.Count; i++) {
                 (int y, int x) = toRemove[i];
-                floor[y, x] = 0;
+                floor[y, x] = false;
             }
             return rolls;
         }
